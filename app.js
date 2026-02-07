8073ee41ed955fffd139ecd7a749995efc3efcc4
dellson-arch <nayanmahato908@gmail.com>
2026-02-07 16:36:30 +0530

    Added my logic of Maximum Subarray

--- app.js
+++ app.js
@@ -39,3 +39,26 @@ var maxProfit = function(prices) {
     }
     return maxProfit
 }
+
+//MAXIMUM SUBARRAY
+//Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+//Output: 6
+//Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+var maxSubArray = function(nums){
+    let sum = nums[0]
+    let maxSum = nums[0]
+
+    for(let i = 0 ; i < nums.length ; i++){
+        if(sum + nums[i] > nums[i]){
+            sum = sum + nums[i]
+        }else{
+            sum = nums[i]
+        }
+
+        if(sum > maxSum){
+            maxSum = sum
+        }
+    }
+    return maxSum
+}
